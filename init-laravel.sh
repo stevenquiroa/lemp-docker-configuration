6e645210d8a3b74c6e61a8e8ae07ef5000608956
Steven Quiroa <stevenkm92@gmail.com>
2017-01-23 10:17:59 -0600

    Update init-laravel.sh

--- init-laravel.sh
+++ init-laravel.sh
@@ -1,4 +1,4 @@
-rm#!/bin/bash
+#!/bin/bash
 
 docker exec ${PWD##*/}_php_1 sh -c 'cd /code && php composer.phar install && cp .env.example .env && php artisan key:generate'
 sudo chown $USER:www-data -R code/bootstrap/cache
